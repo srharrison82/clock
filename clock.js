1fc63394f23914315ceda3592318a3205bb05d53
srharrison82 <srharrison82@gmail.com>
2022-09-13 11:00:51 +0100

    Project done

---
+++ clock.js
@@ -0,0 +1,31 @@
+//Nav
+const toggleButton = document.getElementsByClassName("nav__toggle-btn")[0];
+const links = document.getElementsByClassName("nav__links")[0];
+
+toggleButton.addEventListener("click", function () {
+  links.classList.toggle("active");
+});
+
+//Clock 
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector('[data-hour-hand]');
+const minuteHand = document.querySelector('[data-minute-hand]');
+const secondHand = document.querySelector('[data-second-hand]');
+
+function setClock() {
+    const currentDate = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60;
+    const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+    const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+
+    setRotation(secondHand, secondsRatio);
+    setRotation(minuteHand, minutesRatio);
+    setRotation(hourHand, hoursRatio);
+}
+
+function setRotation (element, rotationRatio) {
+    element.style.setProperty('--rotation', rotationRatio * 360);
+}
+
+setClock();
